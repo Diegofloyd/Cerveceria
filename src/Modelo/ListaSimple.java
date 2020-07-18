e454b6ce818989e5edceb26516cfe99779cc6103
Johan López <Johan Lpez@192.168.1.82>
2020-07-18 17:32:36 -0500

    Modificaciones de los metodos ListaSimple.java

--- src/Modelo/ListaSimple.java
+++ src/Modelo/ListaSimple.java
@@ -82,11 +82,10 @@ public class ListaSimple {
         return resp;
     }
 
-
-    public void Insertar(String Nombre, String Marca, String porcientoAlcohol, int Precio,  String Tamano, String tipoCerveza, String Presentacion) {
+    public void AgregarRegistro(String Nombre, String Marca, String porcientoAlcohol, int Precio, String Tamano, String tipoCerveza, String Presentacion) {
         Nodo nuevo_N = new Nodo();
-         this.cont++;
-         nuevo_N.setNum(cont);
+        this.cont++;
+        nuevo_N.setNum(cont);
         nuevo_N.setNombre(Nombre);
         nuevo_N.setMarca(Marca);
         nuevo_N.setPorcientoAlcohol(porcientoAlcohol);
@@ -94,7 +93,6 @@ public class ListaSimple {
         nuevo_N.setTamano(Tamano);
         nuevo_N.setTipoCerveza(tipoCerveza);
         nuevo_N.setPresentacion(Presentacion);
-        
 
         if (Vacia()) {
             this.inicio = nuevo_N;
@@ -107,7 +105,7 @@ public class ListaSimple {
         }
     }
 
-    public Nodo mostrarLista() {
+    public Nodo mostrarRegistros() {
         Nodo aux = new Nodo();
         aux = EstadoActual;
         if (Vacia()) {
@@ -122,7 +120,7 @@ public class ListaSimple {
                 aux.getTamano();
                 aux.getTipoCerveza();
                 aux.getPresentacion();
-                
+
             }
         }
 
@@ -130,92 +128,82 @@ public class ListaSimple {
         return aux;
     }
 
+    public void Indexar() {
+        Nodo aux = this.inicio;
 
-    public void Enumerar(){
-        Nodo aux =this.inicio;
-        
-       //Contador=Contador-1;
-        if (Vacia()==false){
-            do{
+        cont = cont - 1;
+        if (Vacia() == false) {
+            do {
                 aux.setNum(cont);
-                aux=aux.getSiguiente();               
-            }while(aux!=null);
+                aux = aux.getSiguiente();
+            } while (aux != null);
         }
     }
 
-    public boolean Buscar(int num){
+    public boolean Buscar(int num) {
         Nodo aux = this.inicio;
         boolean r;
-        r=false;
-        if (Vacia()!=false){
-            JOptionPane.showMessageDialog(null,"No hay ningún registro");
-        }
-        else{
-            do{
-               if(aux.getNum()==num) {
-                   r=true;                    
-               }
-               aux=aux.getSiguiente();
-            }while(aux!=null);
+        r = false;
+        if (Vacia() != false) {
+            JOptionPane.showMessageDialog(null, "No hay ningún registro");
+        } else {
+            do {
+                if (aux.getNum() == num) {
+                    r = true;
+                }
+                aux = aux.getSiguiente();
+            } while (aux != null);
         }
         return r;
     }
 
+    public void Eliminar(int n) {
 
+        if (Vacia() == false) {
+            Nodo Actual = this.inicio;
+            Nodo ActBorrar = Actual.getSiguiente();
+            if (Actual.getNum() == n && this.inicio == this.fin) {
+                this.inicio = null;
+                this.fin = null;
+                EstadoActual = null;
+                cont = 0;
+            } else {
+                if (Actual.getNum() == n && Actual == inicio && Actual.getSiguiente() == this.fin) {
+                    this.inicio = this.inicio.getSiguiente();
+                    cont = cont - 1;
+                } else {
+                    if (ActBorrar.getNum() == n && Actual == inicio && Actual.getSiguiente() == this.fin && ActBorrar.getSiguiente() == null) {
+                        Actual.setSiguiente(null);
+                        this.fin = this.inicio;
+                        cont--;
+                    } else {
+                        while (Actual.getSiguiente() != null) {
+                            if (ActBorrar.getNum() == n) {
+                                Nodo BorrarSig = ActBorrar.getSiguiente();
+                                Actual.setSiguiente(BorrarSig);
+                                cont--;
+                                //break;
+                            }
+                            Actual = Actual.getSiguiente();
+                            if (Actual.getSiguiente() != null) {
+                                ActBorrar = Actual.getSiguiente();
+                                cont--;
+                            }
+                        }
 
-    public void Eliminar(int n){
-        
-         
-        if (Vacia()==false){
-           Nodo Actual= this.inicio;
-           Nodo ActBorrar = Actual.getSiguiente();
-           if(Actual.getNum()==n&&this.inicio==this.fin){
-               this.inicio=null;
-               this.fin=null;
-               EstadoActual=null;
-               cont=0;
-           }
-           else{
-               if(Actual.getNum()==n && Actual==inicio &&Actual.getSiguiente()==this.fin){
-                   this.inicio=this.inicio.getSiguiente();
-                   cont=cont-1;
-               }
-               else{
-                   if(ActBorrar.getNum()==n && Actual==inicio && Actual.getSiguiente()==this.fin && ActBorrar.getSiguiente()==null){
-                       Actual.setSiguiente(null);
-                       this.fin=this.inicio;
-                       cont--;
-                   }
-                   else{
-                       while(Actual.getSiguiente()!=null){
-                        if(ActBorrar.getNum()==n){
-                           Nodo BorrarSig = ActBorrar.getSiguiente();
-                           Actual.setSiguiente(BorrarSig);
-                           cont--;
-                           //break;
-                        }
-                        Actual=Actual.getSiguiente();
-                        if(Actual.getSiguiente()!=null){
-                           ActBorrar= Actual.getSiguiente();
-                           cont--;
-                        }
-                       }
-                       
-                   }
-               }
-           }
+                    }
+                }
+            }
         }
     }
 
-
-    public void ModificarRegistro(String Nombre, String Marca, String porcientoAlcohol, int Precio,  String Tamano, String tipoCerveza, String Presentacion, int num){
-        Nodo Aux=this.inicio;
-        if (Vacia()){
-            JOptionPane.showMessageDialog(null,"No hay ningún registro");
-        }
-        else{
-            do{
-                if(Aux.getNum()==num){
+    public void ModificarRegistro(String Nombre, String Marca, String porcientoAlcohol, int Precio, String Tamano, String tipoCerveza, String Presentacion, int num) {
+        Nodo Aux = this.inicio;
+        if (Vacia()) {
+            JOptionPane.showMessageDialog(null, "No hay ningún registro");
+        } else {
+            do {
+                if (Aux.getNum() == num) {
                     Aux.setNombre(Nombre);
                     Aux.setMarca(Marca);
                     Aux.setPorcientoAlcohol(porcientoAlcohol);
@@ -223,63 +211,57 @@ public class ListaSimple {
                     Aux.setTamano(Tamano);
                     Aux.setTipoCerveza(tipoCerveza);
                     Aux.setPresentacion(Presentacion);
-                    
+
                 }
-                Aux=Aux.getSiguiente();
-            }while (Aux!=null);
+                Aux = Aux.getSiguiente();
+            } while (Aux != null);
         }
-    }    
+    }
 
-
-
-
-    public void GuardarArchivo(){
-        try{
+    public void GuardarArchivo() {
+        try {
             Nodo auxiliar = this.inicio;
-            FileWriter fw = new FileWriter("Datos.txt",false);
+            FileWriter fw = new FileWriter("Datos.txt", false);
             BufferedWriter bw = new BufferedWriter(fw);
             PrintWriter pw = new PrintWriter(bw);
-            do{
-                pw.print(auxiliar.getNum()+"|");
-                pw.print(auxiliar.getNombre()+"|");
-                pw.print(auxiliar.getMarca()+"|");
-                pw.print(auxiliar.getPorcientoAlcohol()+"|");
-                pw.print(auxiliar.getPrecio()+"|");
-                pw.print(auxiliar.getTamano()+"|");
-                pw.print(auxiliar.getTipoCerveza()+"|");
-                pw.print(auxiliar.getPresentacion()+"|");
-                
+            do {
+                pw.print(auxiliar.getNum() + "|");
+                pw.print(auxiliar.getNombre() + "|");
+                pw.print(auxiliar.getMarca() + "|");
+                pw.print(auxiliar.getPorcientoAlcohol() + "|");
+                pw.print(auxiliar.getPrecio() + "|");
+                pw.print(auxiliar.getTamano() + "|");
+                pw.print(auxiliar.getTipoCerveza() + "|");
+                pw.print(auxiliar.getPresentacion() + "|");
+
                 pw.println();
-                auxiliar=auxiliar.getSiguiente();
-            }while(auxiliar!=null);
+                auxiliar = auxiliar.getSiguiente();
+            } while (auxiliar != null);
             JOptionPane.showMessageDialog(null, "Los datos han sido almacenados correctamente.");
             pw.close();
             bw.close();
             fw.close();
-        } catch(Exception e){
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "No se ha podido completar la operación. \nIntente de nuevo más tarde");
         }
     }
 
-
-
-
-    public void CargarArchivos(){
-        try{
+    public void CargarArchivos() {
+        try {
             FileReader fr = new FileReader("Datos.txt");
             BufferedReader br = new BufferedReader(fr);
             String line;
             Nodo auxiliar = new Nodo();
-            while((line=br.readLine())!=null){
-                StringTokenizer data = new StringTokenizer(line,"|");
-                int token=0;
-                while(data.hasMoreTokens()){
-                    switch(token){
+            while ((line = br.readLine()) != null) {
+                StringTokenizer data = new StringTokenizer(line, "|");
+                int token = 0;
+                while (data.hasMoreTokens()) {
+                    switch (token) {
                         case 0:
                             auxiliar.setNum(Integer.parseInt(String.valueOf(data.nextToken())));
                             token++;
                             break;
-                        case 1: 
+                        case 1:
                             auxiliar.setNombre(String.valueOf(data.nextToken()));
                             token++;
                             break;
@@ -307,38 +289,37 @@ public class ListaSimple {
                             auxiliar.setPresentacion(String.valueOf(data.nextToken()));
                             token++;
                             break;
-                      
+
                     }
                 }
                 this.Insertar(auxiliar);
-                cont=auxiliar.getNum();
+                cont = auxiliar.getNum();
             }
             JOptionPane.showMessageDialog(null, "Los datos han sigo cargados.");
-        } catch(Exception e){
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "No se ha podido completar la operación. \nIntente de nuevo más tarde");
         }
+    }
+
+    public void Insertar(Nodo aux) {
+        Nodo nvoNodo = new Nodo();
+        nvoNodo.setNum(aux.getNum());
+        nvoNodo.setNombre(aux.getNombre());
+        nvoNodo.setMarca(aux.getMarca());
+        nvoNodo.setPorcientoAlcohol(aux.getPorcientoAlcohol());
+        nvoNodo.setPrecio(aux.getPrecio());
+        nvoNodo.setTamano(aux.getTamano());
+        nvoNodo.setTipoCerveza(aux.getTipoCerveza());
+        nvoNodo.setPresentacion(aux.getPresentacion());
+        if (Vacia()) {
+            this.inicio = nvoNodo;
+            this.fin = nvoNodo;
+            this.EstadoActual = inicio;
+        } else {
+            this.fin.setSiguiente(nvoNodo);
+            this.fin = nvoNodo;
+
         }
 
-
-        public void Insertar(Nodo aux){
-            Nodo nvoNodo = new Nodo();
-            nvoNodo.setNum(aux.getNum());
-            nvoNodo.setNombre(aux.getNombre());
-            nvoNodo.setMarca(aux.getMarca());
-            nvoNodo.setPorcientoAlcohol(aux.getPorcientoAlcohol());
-            nvoNodo.setPrecio(aux.getPrecio());
-            nvoNodo.setTamano(aux.getTamano());
-            nvoNodo.setTipoCerveza(aux.getTipoCerveza());
-            nvoNodo.setPresentacion(aux.getPresentacion());
-            if(Vacia()){
-                this.inicio=nvoNodo;
-                this.fin=nvoNodo;
-                this.EstadoActual=inicio;
-            } else{
-               this.fin.setSiguiente(nvoNodo);
-               this.fin=nvoNodo;
-               
-            }
-            
-        }
+    }
 }
